369009f33577d0c7ae773c97a1ec7e92b60f88a8
unknown <enes118022@gmail.com>
2026-09-16 14:38:40 +0300

    Helmet düzenlendi.

--- src/security/helmet.ts
+++ src/security/helmet.ts
@@ -1,15 +1,31 @@
 import helmet from "helmet";
 
 const helmetConfig = helmet({
-    // HSTS prod nginx (nginx.prod.conf) üzerinden yönetilir; helmet dev http'te HSTS cache oluşturmasın
     hsts: false,
+
     contentSecurityPolicy: {
         directives: {
             defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", "'sha256-6pNZ95ONb/PhdKHLjnUiz7Ufwe8qoTXaGinPK/2QQdY='"],
-            styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
-            fontSrc: ["'self'", "https://fonts.gstatic.com"],
-            imgSrc: ["'self'", "data:", "https://res.cloudinary.com"]
+            scriptSrc: [
+                "'self'",
+                "'sha256-6pNZ95ONb/PhdKHLjnUiz7Ufwe8qoTXaGinPK/2QQdY='"
+            ],
+            styleSrc: [
+                "'self'",
+                "'unsafe-inline'",
+                "https://fonts.googleapis.com"
+            ],
+            fontSrc: [
+                "'self'",
+                "https://fonts.gstatic.com"
+            ],
+            imgSrc: [
+                "'self'",
+                "data:",
+                "https://res.cloudinary.com"
+            ],
+
+            upgradeInsecureRequests: null
         }
     }
 });
